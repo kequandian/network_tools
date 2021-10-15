5bae69a163b678efab390998608a660a6a47abb7
Samuel Yuan <samuelyuan@hotmail.com>
2021-10-15 20:23:38 +0800

    create-indexes.sh

---
+++ dummy-cli.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker-compose -f dummy.yml run --rm dummy bash 
--- dummy-deploy.sh
+++ dummy-deploy.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 # startup: start dummy api; restart: restart container; deploy: deploy only
 if [ -f .env ];then source .env;fi
-export DEPLOY_OPT=deploy
+export DEPLOY_OPT=restart
 
 dependency=$1
 if [ $dependency ];then 
   ./dependency-copy.sh $dependency
 fi
+
+## handle app deploy
+standalone=$(ls *-standalone *.war 2> /dev/null)
+if [ ! -z $standalone ];then
+  for app in $standalone;do
+    echo mv $app ${DUMMY_WORKING_DIR}
+    mv $app ${DUMMY_WORKING_DIR}
+  done   
+fi
+
+########
 
 docker-compose -f dummy.yml --project-name ${DUMMY_PROJECT_NAME}-deploy up --always-recreate-deps
