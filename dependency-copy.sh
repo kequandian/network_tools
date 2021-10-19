f7efc6bf6ba2c9b12522d705a50ea1ae0607329e
vincent@macbook <vincent@macbook.air>
2021-10-19 23:37:14 +0800

    update dependency-copy.sh

--- dependency-copy.sh
+++ dependency-copy.sh
@@ -1,29 +1,98 @@
 #!/usr/bin/env bash
+### start get working_dir
+# if [ -f .env ];then source .env;fi
+# working_dir=${DUMMY_WORKING_DIR}
+workingdir(){
+   if [ ! $DUMMY_CONTAINER ];then
+      if [ -f .env ];then source .env;fi
+   fi
+  #  curl -s http://localhost:2375/containers/${DUMMY_CONTAINER}/json | jq '.HostConfig.Binds[] | match("([a-z/]+):/webapps") | .captures[0].string'
+  #  curl -s http://localhost:2375/containers/${DUMMY_CONTAINER}/json | jq '.HostConfig.Binds[] | match("([a-z/]+):([a-z/]*/webapps[a-z/]*)") | .captures[].string'
+  binds=$(curl -s http://localhost:2375/containers/${DUMMY_CONTAINER}/json | jq '.HostConfig.Binds[] | match("([a-z/]+):[a-z/]*/webapps[a-z/]*").string')
+  local working_dir
+  for bind in $binds;do
+    bind=${bind%\"}
+    bind=${bind#\"}
+    if [[ $bind == *webapps ]];then
+      working_dir=${bind%:*}
+      echo $working_dir
+    fi
+  done
+}
+################################
+getfatjar(){
+  standalone=$(ls $working_dir/*-standalone.jar $working_dir/app.jar $working_dir/*.war 2> /dev/null)
+  if [ -z $standalone ];then
+     exit
+  fi
+  echo $standalone
+}
+################################
+
+## main
+JAR_BIN=$(which jar)
 
 ## find pattern within fatjar
-artifact_get_x(){
+artifact_get(){
    ## get artifact from fatjar
-   echo .
+   pattern=$1
+   if [ ! $pattern ];then
+      echo . > /dev/null
+      exit
+   fi
+
+   working_dir=$(workingdir)
+   if [ -z "$working_dir" ];then 
+      # echo no container: [${DUMMY_CONTAINER}] found ! > /dev/stderr
+      echo $pattern
+      exit
+   fi
+   fatjar=$(getfatjar)
+   if [ -z "$fatjar" ];then 
+      # echo + $working_dir
+      # echo no fatjar found ! > /dev/stderr`
+      echo $pattern
+      exit
+   fi
+
+   # start
+   result=$("$JAR_BIN" tf $fatjar | grep $pattern)
+   num=0
+   for entry in $result;do
+      echo $entry > /dev/stderr
+      num=$(($num+1))
+   done
+
+   if [ $num = 1 ];then 
+      resultok=$("$JAR_BIN" xf $fatjar $result)
+      echo $resultok
+   elif [ $num = 0 ];then
+      echo "no matches \"$pattern\" in $fatjar !" > /dev/stderr  
+   fi
 }
 
-artifact_get() {
+# convert jar-1.0.0.jar to com.jfeat:jar:1.0.0
+artifact_x() {
    local art=$1
+   local group_id='com.jfeat'
    if [ ! $art ];then 
      exit
    fi
    if [[ $art =~ ':' ]];then 
-      echo $art
+      echo $group_id:$art
    else
       local art_name=${art%%-[0-9\.]*.jar}
       local art_version=${art##*-};art_version=${art_version%.jar}
-      echo $art_name:$art_version
+      echo $group_id:$art_name:$art_version
    fi
 }
+
 
 ## main 
 
 artifact_pattern=$1
 artifact=$(artifact_get $artifact_pattern)
+artifact=$(artifact_x $artifact)
 outputdir=$2
 
 if [ ! $outputdir ];then
@@ -47,4 +116,4 @@ if [ ! $artifact ];then
    exit
 fi
 
-./mvn.sh dependency:copy -Dartifact=com.jfeat:$artifact -DoutputDirectory=. 
+./mvn.sh dependency:copy -Dartifact=$artifact -DoutputDirectory=. 
