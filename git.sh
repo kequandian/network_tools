c4ad228bdb57e6d9c3efafeacdf71ef460aaab12
Samuel Yuan <samuelyuan@hotmail.com>
2021-10-20 09:17:48 +0800

    fix git.sh

--- git.sh
+++ git.sh
@@ -10,5 +10,5 @@
 #       - ${HOME}/.ssh/known_hosts:/root/.ssh/known_hosts
 ##############################
 
-# echo docker run --rm -it -v ${PWD}:/git -v ${HOME}/.ssh/known_hosts:/root/.ssh/known_hosts arm32v7/allin-web:git $@
-docker run --rm -it --net=host -v ${PWD}:/git -v ${HOME}/.ssh/known_hosts:/root/.ssh/known_hosts arm32v7/allin-web:git $@
+# docker run --rm -it --net=host -v ${PWD}:/git -v ${HOME}/.ssh/known_hosts:/root/.ssh/known_hosts arm32v7/allin-web:git $@
+docker-compose -f local/yaml/git.yaml run --rm git git $@
