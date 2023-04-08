d881656e24b74ec784c52ac4cea127232a42c6b3
tmp <you@email>
2023-04-08 22:03:03 +0800

    add hello-api.sh, git-wrap.sh

--- mvn-wrap.sh
+++ mvn-wrap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -v $HOME/.m2:/root/.m2 -v $PWD:/usr/src -w /usr/src maven:3.6-adoptopenjdk-11 mvn -DskipStandalone=false clean package
+docker run --rm -v $HOME/.m2:/root/.m2 -v $PWD:/usr/src -w /usr/src maven:3.6-adoptopenjdk-11 mvn -DskipStandalone=false $@ #clean package
