b033043e890c9d8ba3f3daa3ea8968101c62b2d4
tmp <you@email>
2023-04-12 11:54:00 +0800

    update dependency-copy.sh, move dummy-*.sh to jenins_scripts

--- dependency-copy-artifact.sh
+++ dependency-copy-artifact.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 usage(){
-   echo "usage: dependency-copy.sh <groupId:artifact:version[:jar:classifier]>"
+   echo "usage: dependency-copy.sh <groupId:artifact:version[:jar:classifier]> [output]"
    exit
 }
 if [ ! $@ ];then
    usage
 fi
 ARTIFACT=$1
+outputdir=$2
 
 #dir=$(dirname $0)
 #cd $dir
-docker run --rm -v ${PWD}:/usr/src/lib zelejs/allin-web:dependency-copy $ARTIFACT
+docker run --rm -v ${PWD}/$outputdir:/usr/src/lib zelejs/allin-web:dependency-copy $ARTIFACT
